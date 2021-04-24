3dfbfe4d3a6ecf3aa8825b59789032fc22a79bac
hexun <hexun@fenbi.com>
2021-04-24 16:35:59 +0800

    update

---
+++ app/src/main/java/com/alien/touch/browser/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alien.touch.browser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
